6b23a2ba01fdb6009bbccbc04cf6bcefd6c2f7f2
Darklord5562 <mrxmana22@gmail.com>
2021-07-02 12:11:09 +0530

    initial

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var theme = getCookie('ctheme')?getCookie('ctheme'):'light'
+var body = document.body
+if(theme =='dark'){
+  body.classList.add('dark-mode')
+}
+else{
+  body.classList.remove('dark-mode')
+}
+setCookie('ctheme', theme, '30')
+var spinnerBox = document.createElement('span')
+var spinner = document.createElement('span')
+
+spinner.classList.add('spinner')
+spinnerBox.classList.add('spinner-box')
+spinnerBox.appendChild(spinner)
+var theme
+
+function darkModeToggle() {
+  body.classList.toggle('dark-mode')
+  theme = body.classList.contains('dark-mode')?'dark':'light'
+  setCookie('ctheme',theme,'30')
+}
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) {
+  let cookie = {};
+  document.cookie.split(';').forEach(function(el) {
+    let [key, value] = el.split('=');
+    cookie[key.trim()] = value;
+  })
+  return cookie[cookieName];
+}
